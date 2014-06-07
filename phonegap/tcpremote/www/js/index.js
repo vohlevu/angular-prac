ab9ebc546d59b7f481021d3b0385d47c757ec0f5
Vu Vo <vohlevu@gmail.com>
2014-06-07 18:40:31 +0700

    Update code phonegap app

--- phonegap/tcpremote/www/js/index.js
+++ phonegap/tcpremote/www/js/index.js
@@ -16,9 +16,10 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
+function app() {
+	var oApp = this;
 	// The dynamically built HTML pages. In a real-life app, In a real-life app, use Handlerbar.js, Mustache.js or another template engine
-	homePage :
+	var homePage =
 		'<div>' +
 			'<header><h1>Page Slider</h1></header>' +
 			'<div class="body">' +
@@ -28,9 +29,9 @@ var app = {
 					'<li><a href="#page3"><strong>Ripple Bot</strong></a></li>' +
 				'</ul>' +
 			'</div>' +
-		'</div>',
+		'</div>';
 
-	detailsPage :
+	var detailsPage =
 		'<div>' +
 			'<header><a href="#" class="btn">Back</a><h1>Robot</h1></header>' +
 			'<div class="body">' +
@@ -40,98 +41,144 @@ var app = {
 					'<p>{{description}}</p>' +
 				'</div>' +
 			'</div>' +
-		'</div>',
+		'</div>';
 
-	slider : new PageSlider($("#app")),
-	key : "",
-	host : "10.0.2.2",
+	var slider = new PageSlider($("#app"));
+	var key = "";
+	var host = "10.0.2.2";
+	var arrRemoteBtn = [
+		{ id: 'btnPower', img: 'img/btn/power.png' },
+		{ id: 'btnReboot', img: 'img/btn/reboot.png' },
+		{ id: 'btnMute', img: 'img/btn/mute.png' },
+		{ id: 'btnVolDown', img: 'img/btn/voldown.png' },
+		{ id: 'btnVolUp', img: 'img/btn/volup.png' },
+		{ id: 'btnNum1', img: 'img/btn/num1.png' },
+		{ id: 'btnNum2', img: 'img/btn/num2.png' },
+		{ id: 'btnNum3', img: 'img/btn/num3.png' },
+		{ id: 'btnNum4', img: 'img/btn/num4.png' },
+		{ id: 'btnNum5', img: 'img/btn/num5.png' },
+		{ id: 'btnNum6', img: 'img/btn/num6.png' },
+		{ id: 'btnNum7', img: 'img/btn/num7.png' },
+		{ id: 'btnNum8', img: 'img/btn/num8.png' },
+		{ id: 'btnNum9', img: 'img/btn/num9.png' },
+		{ id: 'btnNum0', img: 'img/btn/num0.png' },
+		{ id: 'btnPlay', img: 'img/btn/play.png' },
+		{ id: 'btnMenu', img: 'img/btn/menu.png' },
+		{ id: 'btnUp', img: 'img/btn/up.png' },
+		{ id: 'btnHome', img: 'img/btn/home.png' },
+		{ id: 'btnDelete', img: 'img/btn/delete.png' },
+		{ id: 'btnPause', img: 'img/btn/pause.png' },
+		{ id: 'btnLeft', img: 'img/btn/left.png' },
+		{ id: 'btnOk', img: 'img/btn/ok.png' },
+		{ id: 'btnRight', img: 'img/btn/right.png' },
+		{ id: 'btnCapsnum', img: 'img/btn/capsnum.png' },
+		{ id: 'btnStop', img: 'img/btn/stop.png' },
+		{ id: 'btnInfo', img: 'img/btn/info.png' },
+		{ id: 'btnDown', img: 'img/btn/down.png' },
+		{ id: 'btnEpg', img: 'img/btn/epg.png' },
+		{ id: 'btnMouse', img: 'img/btn/mouse.png' },
+		{ id: 'btnPrev', img: 'img/btn/prev.png' },
+		{ id: 'btnRev', img: 'img/btn/rev.png' },
+		{ id: 'btnNext', img: 'img/btn/next.png' },
+		{ id: 'btnFwd', img: 'img/btn/fwd.png' },
+		{ id: 'btnKeyboard', img: 'img/btn/keyboard.png' },
+		{ id: 'btnRed', img: 'img/btn/red.png' },
+		{ id: 'btnGreen', img: 'img/btn/green.png' },
+		{ id: 'btnYellow', img: 'img/btn/yellow.png' },
+		{ id: 'btnBlue', img: 'img/btn/blue.png' },
+		{ id: 'btnBack', img: 'img/btn/back.png' }
+	];
     // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-		this.route();
-    },
+    oApp.initialize = function() {
+        oApp.bindEvents();
+		oApp.route();
+		arrRemoteBtn = oApp.transform( arrRemoteBtn );
+    };
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        //document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
+    oApp.bindEvents = function() {
+        //document.addEventListener('deviceready', oApp.onDeviceReady, false);
+    };
     // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-	goHome: function(event) {
+    oApp.onDeviceReady = function() {
+        oApp.receivedEvent('deviceready');
+    };
+	oApp.goHome = function(event) {
 		window.location.hash="#";
 		event.preventDefault();
 		return false;
-    },
+    };
 	// Basic page routing
-	route : function(event) {
+	oApp.route = function(event) {
 		var page,
 			hash = window.location.hash;
 
 		if (hash === "#page1") {
-			//page = this.merge(this.detailsPage, {img: "buildbot.jpg", name: "Build Bot", description: "Lorem Ipsum"});
-	//        slider.slide($(page), "right");
 			var source   = $("#remote-template").html();
 			var template = Handlebars.compile(source);
-			page = template();
-			this.connect();
+			page = template(arrRemoteBtn);
+			//oApp.connect();
 		} else if (hash === "#page2") {
-			page = this.merge(this.detailsPage, {img: "medibot.jpg", name: "Medi Bot", description: "Lorem Ipsum"});
-	//        slider.slide($(page), "right");
+			page = oApp.merge(detailsPage, {img: "medibot.jpg", name: "Medi Bot", description: "Lorem Ipsum"});
 		} else if (hash === "#page3") {
-			page = this.merge(this.detailsPage, {img: "ripplebot.jpg", name: "Ripple Bot", description: "Lorem Ipsum"});
-	//        slider.slide($(page), "right");
-		} else if (hash === "#btnPower") {
-			this.send("Send from Phonegap App");
-			return;
+			page = oApp.merge(detailsPage, {img: "ripplebot.jpg", name: "Ripple Bot", description: "Lorem Ipsum"});
 		} else {
-			page = this.homePage;
-	//        slider.slide($(homePage), "left");
+			page = homePage;
 		}
-		this.slider.slidePage($(page));
-	},
-	merge : function(tpl, data) {
+		slider.slidePage($(page));
+		$('a.img-contain').click(function(event){
+			//oApp.send("Send from Phonegap App >> id:" + this.id);
+			//alert(this.id);
+			event.preventDefault();
+		});
+	};
+	oApp.merge = function(tpl, data) {
 		return tpl.replace("{{img}}", data.img)
 				  .replace("{{name}}", data.name)
 				  .replace("{{description}}", data.description);
-	},
+	};
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
+    oApp.receivedEvent = function(id) {
         console.log('Received Event: ' + id);
-    },
-    stub: function(message) {
+    };
+    oApp.stub = function(message) {
 		console.log(message);
-	},
-    connect: function() {
-		window.tlantic.plugins.socket.connect(this.onConnect, this.stub, this.host, 18002);
-	},
-    send: function(data) {
-		window.tlantic.plugins.socket.send(this.stub, this.stub, app.key, data);
-	},
-    onConnect: function(k) {
-		app.key = k;
-	},
-    disconnect: function() {
-		window.tlantic.plugins.socket.disconnect(this.stub, this.stub, app.key);
-	},
-    isConnected: function() {
-		window.tlantic.plugins.socket.isConnected(app.key, this.stub, this.stub);
-	}
+	};
+    oApp.connect = function() {
+		window.tlantic.plugins.socket.connect(oApp.onConnect, oApp.stub, host, 18002);
+	};
+    oApp.send = function(data) {
+		window.tlantic.plugins.socket.send(oApp.stub, oApp.stub, key, data);
+	};
+    oApp.onConnect = function(k) {
+		key = k;
+	};
+    oApp.disconnect = function() {
+		window.tlantic.plugins.socket.disconnect(oApp.stub, oApp.stub, key);
+	};
+    oApp.isConnected = function() {
+		window.tlantic.plugins.socket.isConnected(key, oApp.stub, oApp.stub);
+	};
+	oApp.transform = function ( arr ) {
+		var result = [], temp = [];
+		arr.forEach( function ( elem, i ) {
+			if ( i > 0 && i % 5 === 0 ) {
+				result.push( temp );
+				temp = [];
+			}
+			temp.push( elem );
+		});
+		if ( temp.length > 0 ) {
+			result.push( temp );
+		}
+		return result;
+	};
 };
-$(window).on('hashchange', function(){app.route();});
+$(window).on('hashchange', function(){
+	app.route();
+});
 window.addEventListener('load', function () {
     new FastClick(document.body);
 }, false);
